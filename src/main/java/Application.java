105af9b99a9dba1561995c1193a0d7a774da2e28
LccsBueno <luccas.lima@sptech.school>
2023-10-23 10:51:24 -0300

    Atualizei

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -24,31 +24,27 @@ public class Application {
             if(args.length > 2){
                 String SETUP = args[0];
                 if(SETUP.toUpperCase().equals("SETUP")){
-                    System.out.println("Entrou no setup()");
                     setup();
                 }
                 String USER_DATABASE = args[1];
                 String PASSWORD_DATABASE = args[2];
-                STATISTICS_REPOSITORY.setConnectionMySql(new ConnectionMySql("admin", "admin"));
+                STATISTICS_REPOSITORY.setConnectionMySql(new ConnectionMySql(USER_DATABASE, PASSWORD_DATABASE));
             } else {
                 String USER_DATABASE = args[0];
                 String PASSWORD_DATABASE = args[1];
-                STATISTICS_REPOSITORY.setConnectionMySql(new ConnectionMySql("admin", "admin"));
+                STATISTICS_REPOSITORY.setConnectionMySql(new ConnectionMySql(USER_DATABASE, PASSWORD_DATABASE));
             }
         }
         Statistics statistics = new Statistics();
-        System.out.println("Entrei nas statistics");
         Util.setInterval(() -> {
-            System.out.println(looca.getSistema());
-            System.out.println(looca.getProcessador());
-            //statistics.setComputer(computer);
-            //statistics.setTemperature(looca.getTemperatura().getTemperatura());
-            //statistics.setCpuUsage(looca.getProcessador().getUso());
-            //statistics.setRamUsage(looca.getMemoria().getEmUso().doubleValue());
-            //statistics.setRamAvailable(looca.getMemoria().getDisponivel().doubleValue());
-            //statistics.setRamTotal(looca.getMemoria().getTotal().doubleValue());
-            //statistics.setDiskTotal(looca.getGrupoDeDiscos().getTamanhoTotal().doubleValue());
-            //statistics.setDiskUsage(looca.getGrupoDeDiscos().getTamanhoTotal().doubleValue());
+            statistics.setComputer(computer);
+            statistics.setTemperature(looca.getTemperatura().getTemperatura());
+            statistics.setCpuUsage(looca.getProcessador().getUso());
+            statistics.setRamUsage(looca.getMemoria().getEmUso().doubleValue());
+            statistics.setRamAvailable(looca.getMemoria().getDisponivel().doubleValue());
+            statistics.setRamTotal(looca.getMemoria().getTotal().doubleValue());
+            statistics.setDiskTotal(looca.getGrupoDeDiscos().getTamanhoTotal().doubleValue());
+            statistics.setDiskUsage(looca.getGrupoDeDiscos().getTamanhoTotal().doubleValue());
 
             STATISTICS_REPOSITORY.save(statistics);
         }, 2, TimeUnit.SECONDS);
@@ -56,7 +52,6 @@ public class Application {
 
     // ATENÇÃO: rode esse método apenas uma vez na main
     public static void setup(){
-        System.out.println("Ta chegando no setup do looca");
         Looca looca = new Looca();
         Computer computer = new Computer();
         Cpu cpu = new Cpu(looca.getProcessador().getId(), (looca.getProcessador().getNome()));
--- src/main/java/repositories/ConnectionMySql.java
+++ src/main/java/repositories/ConnectionMySql.java
@@ -11,8 +11,8 @@ public class ConnectionMySql {
     private String password;
 
     public ConnectionMySql(){
-        this.user = "admin";
-        this.password = "admin";
+        this.user = "aluno";
+        this.password = "aluno";
     }
 
     public ConnectionMySql(String user, String password){
